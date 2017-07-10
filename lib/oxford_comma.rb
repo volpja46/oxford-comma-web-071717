7657a752e980c9f44238674d819a72246a08c9d2
Jennifer Volpe <jennifervolpe@Jennifers-MBP.home>
2017-07-10 19:20:00 -0400

    commas

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
 
+if array.length < 2
+   array.join
+   
+ elsif array.length == 2
+   array.insert(1, "and").join(" ")
+   else
+    str = ", and " + array.pop
+    str = array.join(", ") << str
+ end
 end
